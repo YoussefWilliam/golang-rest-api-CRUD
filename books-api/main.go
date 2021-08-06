81f1eee33d962cafbc905c036912ce89a3c2814d
YoussefWilliam <youssef.william997@gmail.com>
2021-08-06 15:26:20 +0200

    Update a book

--- books-api/main.go
+++ books-api/main.go
@@ -57,3 +57,24 @@ func createBook(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(book)	
 
 }
+// Update a book
+func updateBook(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	for index,item := range books {
+		if(item.ID == params["id"]){
+			books = append(books[:index],books[index+1:]...)
+			break
+		}
+	}
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+
+
+	book.ID = params["id"]
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)	
+
+}
