30181ff4399d1880b7520754308c7096885f048c
YoussefWilliam <youssef.william997@gmail.com>
2021-08-06 15:26:08 +0200

    Create a book

--- books-api/main.go
+++ books-api/main.go
@@ -44,3 +44,16 @@ func getBook(w http.ResponseWriter, r *http.Request){
 	}
 	json.NewEncoder(w).Encode(&Book{})
 }
+// Create a book
+
+func createBook(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+
+
+	book.ID = strconv.Itoa(rand.Intn(100))
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)	
+
+}
